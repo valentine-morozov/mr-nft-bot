c67bffff8ade87413334c76042bea205a0e9f9e7
valentin <sendto@bk.ru>
2021-05-30 01:08:37 +0300

    photos

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
+const axios = require('axios');;const { log } = require('console');
+
 
 const telegramToken = '1733621272:AAEwqYpCYYhZEYQkh4IrGLgM-vuAz9X_sMg';
 const covalentApiKey = 'ckey_dd18b4e15a8e42ccbeee39754d3';
+const host = 'https://api.covalenthq.com';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(telegramToken, {polling: true});
@@ -18,24 +20,43 @@ Send me your *BEP20 address* and I will show you your *NFTs*
     bot.sendMessage(msg.chat.id, text, {parse_mode: 'markdown'});
 });
 
-bot.onText(/^0x([\w\d]+)/, (msg, match) => {
+bot.onText(/^0x([\w\d]+)/, async (msg, match) => {
     const address = match.input;
-    const networkId = 56;
+    const chainId = 56;
     
-    const url = `https://api.covalenthq.com/v1/${networkId}/address/${address}/balances_v2/?key=${covalentApiKey}&nft=true`
+    const getBalancesURL = `${host}/v1/${chainId}/address/${address}/balances_v2/?key=${covalentApiKey}&nft=true`;
 
-    axios.get(url)
-        .then(res => {
-            const body = res.data;
+    try {
+        const res = await axios.get(getBalancesURL);
+
+        const body = res.data;
             if (!body.error) {
                 const tokens = body.data.items;
-                const nfts = tokens.filter(token => token.type == 'nft');
-                console.log(nfts);
+                const tokensNFTs = tokens.filter(token => token.type == 'nft');
+                if (tokensNFTs.length) {
+                    bot.sendMessage(msg.chat.id, '*Your NFTs:*', {parse_mode: 'markdown'});
+                    for (token of tokensNFTs) {
+                        // NFT is created on JGN-NFT
+                        if (token.contract_name == 'JGNNFT') {
+                            for (nft of token.nft_data) {
+                                let caption = `*${nft.external_data.name}*`;
+                                if (nft.external_data.description) {
+                                    caption += "\n${nft.external_data.description}"
+                                }
+                                bot.sendPhoto(msg.chat.id, nft.external_data.image, {
+                                    caption: caption,
+                                    parse_mode: 'markdown'
+                                });
+                            }
+                        }
+                    }
+                } else {
+                    bot.sendMessage("You don't have any supported NFTs :(");
+                }
             } else {
                 bot.sendMessage(msg.chat.id, "Sorry, I can't get information for your address")
             }
-        })
-        .catch(err => {
-            console.log('Error: ', err.message);
-        });
+    } catch (err) {
+        console.log('Error: ', err.message);
+    }
 })
